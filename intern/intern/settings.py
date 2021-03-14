e39d0238d59b32e1ec2510aea5ecfa750f0969c5
Samarth vyas <svsamarth555@gmail.com>
2021-03-15 00:56:30 +0530

    updated

--- intern/intern/settings.py
+++ intern/intern/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
